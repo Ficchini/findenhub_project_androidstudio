bca3e961ad24bb88ecd6c7fb0f420abf53c12e67
JFicchini <jficchini.dev@gmail.com>
2026-05-30 12:36:57 -0300

    update: creating the java/xml structure

---
+++ app/src/main/java/com/findenhub_project/app/ui/supplier/services/CreateServiceActivity.java
@@ -0,0 +1,26 @@
+package com.findenhub_project.app.ui.supplier.services;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.findenhub_project.app.R;
+
+public class CreateServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_service);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
